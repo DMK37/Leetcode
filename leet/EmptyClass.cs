c0d2714eec72a86335d469898d04faaf169027f8
Dmytro Tolstoi <dima@Dmytros-Macbook13.local>
2023-02-11 12:11:16 +0100

    TwoSum, LengthOfLongestSubstring

---
+++ leet/EmptyClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace leet
+{
+	public static class Solution
+	{
+		public static int[]? TwoSum(int[] nums, int target)
+		{
+			Dictionary<int, int> compelements = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				if (compelements.ContainsKey(target - nums[i]))
+				{
+					return new int[] { i, compelements[target - nums[i]] };
+				}
+				if (!compelements.ContainsKey(nums[i]))
+					compelements.Add(nums[i], i);
+			}
+			return null;
+		}
+
+		public static int LengthOfLongestSubstring(string s)
+		{
+			if (s.Length == 0)
+				return 0;
+			Dictionary<int,int> sub_str = new Dictionary<int, int> { };
+			int l = 0;
+			int r = 0;
+			int len = 1;
+			while(r < s.Length)
+			{
+				if (sub_str.ContainsKey(s[r]))
+				{					
+					while (s[l] != s[r])
+					{
+						sub_str.Remove(s[l]);
+						l++;
+					}
+                    sub_str.Remove(s[l]);
+					l++;
+                }
+				sub_str.Add(s[r], r);
+				len = int.Max(len, r - l + 1);
+				r++;
+			}
+			return len;
+		}
+
+        public static int MyAtoi(string s)
+        {
+			if (s.Length == 0)
+				return 0;
+			int num = 0;
+			int size = 0;
+			bool IsPlus = true;
+			double max = int.MaxValue;
+			while(size < s.Length)
+			{
+				if (s[size] >= '0' && s[size] <= '9')
+				{
+					
+                    num *= 10;
+                    if (IsPlus)
+						num += s[size] - 48;
+					else
+                        num -= s[size] - 48;
+				}
+				else
+				{
+					if (s[size] == '-')
+						IsPlus = false;
+					else
+					if (s[size] == '+') { }
+					else
+					if (s[size] == ' ') { }
+					else
+						break;
+                }
+				size++;
+				
+			}
+			return num;
+        }
+    }
+}
+
---
+++ leet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 18;
+            int[]? res = Solution.TwoSum(nums, target);
+            foreach(int num in res)
+            {
+                Console.WriteLine(num);
+            }
+
+            string s = "abdnjksdskssfffsf";
+            Console.WriteLine(Solution.LengthOfLongestSubstring(s));
+            Console.WriteLine(Solution.MyAtoi("4193 with words"));
+        }
+    }
+}
+
+
